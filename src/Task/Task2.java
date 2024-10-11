8298f6a3ee6d3eb23c95260ce502c40f9d2c5ab0
Arjun Reddy <arjun.putluri@gmail.com>
2024-10-11 14:14:58 +0530

    My Files

---
+++ src/Task/Task2.java
@@ -0,0 +1,11 @@
+package Task;
+
+public class Task2 {
+    public static void main(String[] args) {
+        //Simple Calculator using SOP Statements
+        System.out.println("Sum of 10 and 3 is : " +(10+3));
+        System.out.println("Subraction of 10 and 3 is : " +(10-3));
+        System.out.println("Multiplication of 10 and 3 is : " +(10*3));
+        System.out.println("Division of 10 and 3 is : " +(10/3));
+    }
+}
