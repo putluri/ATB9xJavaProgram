15103086e6f250687cea86c3237d2bbadff42fd6
Arjun Reddy <arjun.putluri@gmail.com>
2024-10-12 07:57:32 +0530

    Push all 2 files

---
+++ src/Oct/ex_11102024/Lab012.java
@@ -0,0 +1,11 @@
+package Oct.ex_11102024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+        int age =10;
+        System.out.printf("Your age is %d", age);
+
+        String name = "Arjun";
+        System.out.printf("Your name is %s",name);
+    }
+}
