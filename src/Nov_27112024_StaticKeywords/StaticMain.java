91ea3477eb487407e9db50e213191e756534dbf4
Arjun Reddy <arjun.putluri@gmail.com>
2024-11-27 15:20:31 +0530

    Commit all 3 changes

---
+++ src/Nov_27112024_StaticKeywords/StaticMain.java
@@ -0,0 +1,17 @@
+package Nov_27112024_StaticKeywords;
+
+public class StaticMain {
+    public static void main(String[] args) {
+        //1. Static method can access static stuff directly ( with out object)
+        System.out.println(StaticDemo.a);
+        StaticDemo.m1();
+        // System.out.println(b); // cannot access, because b is non-static variable
+        // m2(); // cannot access, because m2 is non-static method
+
+        //2. Static method can also access non-static stuff through object
+        StaticDemo sd = new StaticDemo();
+        System.out.println(sd.b);
+        sd.m2();
+        sd.m();
+    }
+}
---
+++ src/Nov_27112024_StaticKeywords/ThisKeyword.java
@@ -0,0 +1,24 @@
+package Nov_27112024_StaticKeywords;
+
+public class ThisKeyword {
+    int x,y;  // class variables or instance variables
+      /* ThisKeyword(int x, int y){
+         this.x=x;
+         this.y=y;
+    } */
+     void setData(int a, int b){
+        x=a;
+        y=b;
+    }
+    void display(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+    public static void main(String[] args) {
+         //  ThisKeyword th = new ThisKeyword(100,200);
+        ThisKeyword th = new ThisKeyword();
+        th.setData(100,200);
+        th.display();
+
+    }
+}
